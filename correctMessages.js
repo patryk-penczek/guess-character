0271fed64829943eba2f2f77ac177fd99474d9c1
Desparek <desparvek@gmail.com>
2023-07-31 09:41:33 +0200

    feat: Add arrays for correct and incorrect guess messages

---
+++ correctMessages.js
@@ -0,0 +1,19 @@
+const correctMessages = [
+  "Impressive work!",
+  "Outstanding!",
+  "You did it!",
+  "Great job!",
+  "Well done!",
+  "Fantastic effort!",
+  "You're on fire!",
+  "Exceptional performance!",
+  "Brilliant!",
+  "Bravo!",
+  "Outstanding!",
+  "Great response!",
+  "Amazing work!",
+  "You got it!",
+  "Keep it up!",
+];
+
+export default correctMessages;
---
+++ incorrectMessages.js
@@ -0,0 +1,19 @@
+const incorrectMessages = [
+  "Wrong character!",
+  "Incorrect answer!",
+  "Bad answer!",
+  "Mistake!",
+  "Incorrect response!",
+  "Unfortunately, not the right character!",
+  "Oops, wrong character!",
+  "Try again!",
+  "Not quite there!",
+  "Close, but not quite!",
+  "Keep practicing!",
+  "You're getting warmer!",
+  "Not the answer we're looking for!",
+  "Not quite on target!",
+  "That's not it, but don't give up!",
+];
+
+export default incorrectMessages;
